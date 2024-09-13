d49fadba73137f87e96acb577a2493e1f0f3d98c
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-09-13 16:03:52 -0400

    build(project): set up initial `Maven` project structure

---
+++ src/main/java/teran/towers/hr/management/api/HrManagementApiApplication.java
@@ -0,0 +1,13 @@
+package teran.towers.hr.management.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teran/towers/hr/management/api/HrManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package teran.towers.hr.management.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
